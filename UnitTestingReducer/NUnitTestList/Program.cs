e2fa52e1a4ef5291c273bb18d1a0073f22d72963
Dmitry Bobrov <me@dbobrov.com>
2015-06-19 13:58:23 +0300

    Test lister done

--- UnitTestingReducer/NUnitTestList/Program.cs
+++ UnitTestingReducer/NUnitTestList/Program.cs
@@ -1,11 +1,34 @@
 ﻿namespace NUnitTestList {
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+
+    using NUnit.Framework;
+
     public class Program {
         public static void Main(string[] args) {
             if (args.Length < 1)
             {
-                
+                Console.Error.WriteLine("usage: NUnitTestList DLL1 DLL2 ...");
+                return;
             }
 
+            foreach (var dll in args)
+            {
+                var assembly = Assembly.LoadFile(Path.GetFullPath(dll));
+                var testMethods = assembly.GetTypes().SelectMany(a => a.GetMethods()).Where(HasTestAttribute);
+                foreach (var method in testMethods)
+                {
+                    Console.WriteLine(method.DeclaringType.FullName + "." + method.Name);
+                }
+            }
+        }
+
+        private static bool HasTestAttribute(MethodInfo m)
+        {
+            var attributes = m.GetCustomAttributes(typeof(TestAttribute));
+            return attributes != null && attributes.Any();
         }
     }
 }
