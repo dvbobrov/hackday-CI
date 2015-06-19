2118158a6919a08618b1564b1126eaed1184a07c
Dmitry Bobrov <me@dbobrov.com>
2015-06-19 14:06:43 +0300

    Sample project

---
+++ UnitTestingReducer/TestAssembly/FooTest.cs
@@ -0,0 +1,24 @@
+﻿namespace TestAssembly
+{
+    using NUnit.Framework;
+
+    using TestedAssembly;
+
+    [TestFixture]
+    public class FooTest
+    {
+        [Test]
+        public void BarPositive()
+        {
+            var foo = new Foo();
+            Assert.AreEqual(1, foo.Bar(1));
+        }
+
+        [Test]
+        public void BarNegative([Values(-1, -2, -10)] int val)
+        {
+            var foo = new Foo();
+            Assert.AreEqual(-val, foo.Bar(val));
+        }
+    }
+}
---
+++ UnitTestingReducer/TestedAssembly/Foo.cs
@@ -0,0 +1,15 @@
+﻿namespace TestedAssembly
+{
+    public class Foo
+    {
+        public int Bar(int x)
+        {
+            if (x < 0)
+            {
+                return -x;
+            }
+
+            return x;
+        }
+    }
+}
